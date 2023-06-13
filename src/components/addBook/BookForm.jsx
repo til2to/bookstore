d979fc664175e567271967b755a331658193a4b8
til2to <padwusup@gmail.com>
2023-06-13 15:17:05 +0000

    user can add book

--- src/components/addBook/BookForm.jsx
+++ src/components/addBook/BookForm.jsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import './addBook.css';
+import { addBook } from '../../redux/books/booksSlice';
+import { generateUniqueId } from '../navigationsLinks';
 
-const BookForm = () => (
-  <div className="container">
-    <div className="title">ADD NEW BOOK</div>
-    <form className="book-form" onSubmit={() => {}}>
-      <input type="text" placeholder="Book title" />
-      <select className="drop-down" disabled>
-        <option value="">Author</option>
-        <option value="non-fiction">Frank Herbert</option>
-        <option value="fantasy">Suzanne Collins</option>
-      </select>
-      <button type="submit">ADD BOOK</button>
-    </form>
-  </div>
-);
+const BookForm = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const handleAddBook = (e) => {
+    e.preventDefault();
+
+    const newBook = {
+      item_id: generateUniqueId(),
+      title,
+      author,
+      category: 'Fiction',
+    };
+    dispatch(addBook(newBook));
+
+    // Reset the form inputs
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <div className="container">
+      <div className="title">ADD NEW BOOK</div>
+      <form className="book-form" onSubmit={handleAddBook}>
+        <input
+          type="text"
+          value={title}
+          placeholder="Book title"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <select
+          className="drop-down"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        >
+          <option value="">Author</option>
+          <option value="John Smith">John Smith</option>
+          <option value="Leo Tolstoy">Leo Tolstoy</option>
+          <option value="Richard Dawkins">Richard Dawkins</option>
+        </select>
+        <button type="submit">ADD BOOK</button>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
--- src/components/books/BookList.jsx
+++ src/components/books/BookList.jsx
@@ -1,32 +1,21 @@
 // import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Book from './Book';
 import BookForm from '../addBook/BookForm';
 import './book-list.css';
-import { addBook } from '../../redux/books/booksSlice';
+import { generateUniqueId } from '../navigationsLinks';
 
-let counter = 0;
+// let counter = 0;
 
-// generate unique numbers
-const generateUniqueId = () => {
-  counter += 1;
-  return `menu-item-${counter}`;
-};
+// // generate unique numbers
+// const generateUniqueId = () => {
+//   counter += 1;
+//   return `menu-item-${counter}`;
+// };
 
 const BookList = () => {
-  const dispatch = useDispatch();
   const { books } = useSelector((store) => store.books);
-
-  const handleAddBook = () => {
-    const newBook = {
-      item_id: generateUniqueId(),
-      title: 'New Book',
-      author: 'New Author',
-      category: 'Fiction',
-    };
-    dispatch(addBook(newBook));
-  };
 
   return (
     <>
@@ -39,9 +28,6 @@ const BookList = () => {
           </ul>
         )
           : 'No books'}
-      </div>
-      <div className="buttons-container">
-        <button type="button" onClick={handleAddBook}>Add Book</button>
       </div>
       <BookForm />
     </>
